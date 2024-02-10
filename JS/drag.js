52a287b231e3c8269b0166bf7efe8b076ff584f1
MinxunC <136438052+MinxunC@users.noreply.github.com>
2024-02-10 09:04:20 +0000

    Update drag.js

--- JS/drag.js
+++ JS/drag.js
@@ -1,9 +1,1 @@
-const draggables = document.querySelectorAll(".draggable");
-const containers = ducument.querySelectorAll(".container");
 
-draggables.forEach(draggable => {
-  draggable.addEventListener("dragstart",(event)=>{
-    draggable.classList.add("dragging")
-  }
-  )
-})
